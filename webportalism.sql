c612c887aa845953c27d29f3526a533eb7261bdc
Prayank Mathur <prayank27mathur@gmail.com>
2016-04-28 21:00:25 +0530

    Added new database file

--- webportalism.sql
+++ webportalism.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.5.45, for Win32 (x86)
 --
 -- Host: localhost    Database: webportalism
 -- ------------------------------------------------------
--- Server version	5.6.27-log
+-- Server version	5.5.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,16 +14,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `webportalism`
---
-
-/*!40000 DROP DATABASE IF EXISTS `webportalism`*/;
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `webportalism` /*!40100 DEFAULT CHARACTER SET utf8 */;
-
-USE `webportalism`;
 
 --
 -- Table structure for table `data`
@@ -45,10 +35,11 @@ CREATE TABLE `data` (
   `DOB` varchar(45) DEFAULT NULL,
   `AddLine1` varchar(100) DEFAULT NULL,
   `AddLine2` varchar(100) DEFAULT NULL,
-  `AddCity` varchar(45) DEFAULT NULL,
-  `AddState` varchar(50) DEFAULT NULL,
-  `AddCountry` varchar(50) DEFAULT NULL,
-  `AddPin` varchar(50) DEFAULT NULL,
+  `City` varchar(50) NOT NULL,
+  `State` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `Pin` varchar(50) NOT NULL,
+  `ImageLink` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`EmailId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -57,9 +48,11 @@ CREATE TABLE `data` (
 -- Dumping data for table `data`
 --
 
+LOCK TABLES `data` WRITE;
 /*!40000 ALTER TABLE `data` DISABLE KEYS */;
-INSERT INTO `data` VALUES ('69saurabh18@gmail.com','saras','Saurabh','Saraswat','K P Saraswat','8797900357','2014-19','Male','Sudha Saraswat','06/01/1995','xyz','qbgfwd','kanpur','up','india','231458'),('aashishkumar96.akr@gmail.com','qazwsxedc1','Aashish','Kumar','Rajesh Kumar','9572383417','2014-18','Male','Sangeeta','14\\12\\1996','3- C- 05','V.V. COLONY','SHAKTINAGAR','UTTAR PRADESH','INDIA','231222');
+INSERT INTO `data` VALUES ('69saurabh18@gmail.com','saras','Saurabh','Saraswat','K P Saraswat','8797900357','2014-19','Male','Sudha Saraswat','06/01/1995','xyz','qbgfwd','kanpur','up','india','231458',NULL),('aashishkumar96.akr@gmail.com','qazwsxedc1','Aashish','Kumar','Rajesh Kumar','9572383417','2014-18','Male','Sangeeta','14\\12\\1996','3- C- 05','V.V. COLONY','SHAKTINAGAR','UTTAR PRADESH','INDIA','231222',NULL),('ayushckc@gmail.com','advancement2512','Ayush','Khare','Ashok Khare','8674806454','2016','Male','Archana Khare','','','','','','','',NULL),('prayank27mathur@gmail.com','ajaymanan1','Prayank','Mathur','Ajay Mathur','8674806463','2016','Male','','','','','','','','',NULL);
 /*!40000 ALTER TABLE `data` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `newsfeed`
@@ -69,20 +62,23 @@ DROP TABLE IF EXISTS `newsfeed`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `newsfeed` (
-  `EmailID` varchar(50) NOT NULL,
-  `nf_id` int(9) NOT NULL AUTO_INCREMENT,
-  `nf_title` varchar(50) NOT NULL,
-  `nf_description` longtext NOT NULL,
-  PRIMARY KEY (`nf_id`,`EmailID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `FeedNumber` int(11) NOT NULL AUTO_INCREMENT,
+  `EmailId` varchar(50) DEFAULT NULL,
+  `FeedText` text,
+  `TimeStamp` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`FeedNumber`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `newsfeed`
 --
 
+LOCK TABLES `newsfeed` WRITE;
 /*!40000 ALTER TABLE `newsfeed` DISABLE KEYS */;
+INSERT INTO `newsfeed` VALUES (15,'69saurabh18@gmail.com','I would love to inform you people that there is an opening in the company Microsoft India Development Center','4/28/2016 5:56:50 PM');
 /*!40000 ALTER TABLE `newsfeed` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `newsfeedcomments`
@@ -104,8 +100,10 @@ CREATE TABLE `newsfeedcomments` (
 -- Dumping data for table `newsfeedcomments`
 --
 
+LOCK TABLES `newsfeedcomments` WRITE;
 /*!40000 ALTER TABLE `newsfeedcomments` DISABLE KEYS */;
 /*!40000 ALTER TABLE `newsfeedcomments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `qualification`
@@ -129,34 +127,11 @@ CREATE TABLE `qualification` (
 -- Dumping data for table `qualification`
 --
 
+LOCK TABLES `qualification` WRITE;
 /*!40000 ALTER TABLE `qualification` DISABLE KEYS */;
 INSERT INTO `qualification` VALUES (6,'aashishkumar96.akr@gmail.com','Facebook','developer',2013,2018),(7,'aashishkumar96.akr@gmail.com','google','Developer',2006,2012),(8,'69saurabh18@gmail.com','amazon','developer',2018,2019),(9,'69saurabh18@gmail.com','flipkart','Head developer',2006,2012);
 /*!40000 ALTER TABLE `qualification` ENABLE KEYS */;
-
---
--- Table structure for table `querydata`
---
-
-DROP TABLE IF EXISTS `querydata`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `querydata` (
-  `EmailID` varchar(100) NOT NULL,
-  `Q_Title` varchar(250) NOT NULL,
-  `Q_Description` longtext NOT NULL,
-  `Q_Type` varchar(50) NOT NULL,
-  `Q_ID` int(9) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`Q_ID`,`EmailID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `querydata`
---
-
-/*!40000 ALTER TABLE `querydata` DISABLE KEYS */;
-INSERT INTO `querydata` VALUES ('69saurabh18@gmail.com','kchas','niojcaslacnkalc\r\nvmlkds\r\nckldjc','Company Related',1),('69saurabh18@gmail.com','asnc','msnlcsdncasdkcnascmacm;lka','Company Related',2);
-/*!40000 ALTER TABLE `querydata` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -167,4 +142,4 @@ INSERT INTO `querydata` VALUES ('69saurabh18@gmail.com','kchas','niojcaslacnkalc
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-09 12:18:29
+-- Dump completed on 2016-04-28 20:58:14
